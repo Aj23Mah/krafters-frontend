3d840b12b7b958d9c479841a4e9dfe3775a895ba
Bijay Maharjan <maharjanbj786@gmail.com>
2023-12-08 20:22:13 +0545

    add: footer

--- src/components/modules/Landing/Hero.tsx
+++ src/components/modules/Landing/Hero.tsx
@@ -3,7 +3,21 @@
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <>
+      <section className="hero-section">
+        <div className="hero-container">
+          <div className="text-area">
+            <h3>Unlock Your IT Potential</h3>
+            <h1>Be Work Ready</h1>
+            <p>Explore Krafter's selection of React courses to acquire proficiency in coding with this highly valuable language.</p>
+            <button>Explore Krafters</button>
+          </div>
+          <div className="image-area">
+            <img src="https://www.pexels.com/photo/women-using-laptop-3277806/" alt="office1" />
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
--- src/components/partials/Footer.tsx
+++ src/components/partials/Footer.tsx
@@ -1,9 +1,90 @@
-
+import  logo  from '../../assets/images/krafters-logo.png'
+import  vector  from '../../assets/images/Vector.png'
+import { CiMobile2 } from "react-icons/ci";
+import { CiPhone } from "react-icons/ci";
+import { CiLocationOn } from "react-icons/ci";
+import { PiEnvelopeSimpleThin } from "react-icons/pi";
+import { SlSocialFacebook } from "react-icons/sl";
+import { SiInstagram } from "react-icons/si";
 
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <section className="footer-section h-auto w-full bg-gray-300 pl-32 pr-32 pt-20 pb-20">
+        <div className="footer-container h-full w-full">
+          <div className="footer-content flex justify-between flex-wrap  border-b pb-6">
+            <div className="footer-box">
+              <div className="image h-20 w-40 mb-8">
+                <img src={logo} alt="logo" className="w-full h-full"/>
+              </div>
+              <div className='info text-sm font-normal'>
+                <div className='flex mb-3'>
+                  <div className='text-lg font-medium mr-3'>
+                    <CiMobile2 />
+                  </div>
+                  <div>
+                    +977-9841809752
+                  </div>
+                </div>
+                <div className='flex mb-3'>
+                  <div className='text-lg font-medium mr-3'>
+                    <CiLocationOn />
+                  </div>
+                  <div className='w-52'>
+                  B&D Educational Castle 4th floor Kumaripati, Lalitpur 
+                  </div>
+                </div>
+                <div className='flex mb-3'>
+                  <div className='text-lg font-medium mr-3'>
+                    <CiPhone />
+                  </div>
+                  <div>
+                    01-4264984
+                  </div>
+                </div>
+                <div className='flex mb-3'>
+                  <div className='text-lg font-medium mr-3'>
+                    <PiEnvelopeSimpleThin />
+                  </div>
+                  <div>
+                    krafters.technology@gmail.com 
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="footer-box text-sm font-normal">
+              <div className='text-lg font-medium mb-2'>QUICK LINKS</div>
+              <div className='mb-2'>About Us</div>
+              <div className='mb-2'>Work With Us</div>
+              <div className='mb-2'>Meet the Team</div>
+              <div className='mb-2'>Vacancies</div>
+            </div>
+            <div className="footer-box text-sm font-normal">
+              <div  className='text-lg font-medium mb-2'>Navigation</div>
+              <div className='mb-2'>Home</div>
+              <div className='mb-2'>Categories</div>
+              <div className='mb-2'>Explore</div>
+              <div className='mb-2'>Contacts</div>
+            </div>
+            <div className="footer-box text-sm font-normal">
+              <div  className='text-lg font-medium mb-2'>NEED HELP?</div>
+              <div className='mb-2'>FAQ</div>
+              <div className='mb-2'>connect with us</div>
+              <div className='mb-2'>Contacts</div>
+            </div>
+          </div>
+          <div className='flex justify-between flex-wrap pt-7'>
+            <div>All Rights Reserved | 2024 | RightFind</div>
+            <div className='flex'>
+              <div className='text-lg font-medium pr-4'><SlSocialFacebook /></div>
+              <div className='text-lg font-medium pr-4'><SiInstagram /></div>
+              <div className='h-4 w-4'><img src={vector} alt="vector" className="w-full h-full"/></div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   )
 }
 
