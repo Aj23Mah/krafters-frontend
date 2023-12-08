0fdc7eab8a8a28a7faeac1a237bd77280c0c9507
Aj23Mah <ajaymaharjan758@gmail.com>
2023-12-08 20:01:52 +0545

    added: responsive

--- src/components/partials/Nav.tsx
+++ src/components/partials/Nav.tsx
@@ -11,62 +11,81 @@ import { BiSolidCategoryAlt } from "react-icons/bi";
 
 const Nav = () => {
   const navItems = [
-    { label: "Categories", icon: <BiSolidCategoryAlt />, path: "/category" },
-    { label: "Explore", icon: <MdOutlineExplore />, path: "/explore" },
-    { label: "Contact", icon: <IoMdContacts />, path: "/contact" },
+    {
+      label: "Categories",
+      icon: <BiSolidCategoryAlt size={28} />,
+      path: "/category",
+    },
+    {
+      label: "Explore",
+      icon: <MdOutlineExplore size={28} />,
+      path: "/explore",
+    },
+    { label: "Contact", icon: <IoMdContacts size={28} />, path: "/contact" },
   ];
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSearchOpen, setIsSearchOpen] = useState(false);
 
-  const handleSearchClick = () => {
-    setIsSearchOpen(!isSearchOpen);
-  };
-
   return (
     <nav className="shadow-sm w-full fixed top-0 left-0 z-50">
-      <div className="md:flex items-center justify-between bg-gray-200 py-2 md:px-20 px-7">
+      <div className="flex items-center justify-between bg-gray-200 py-2 md:px-20 px-7">
         <div className="mr-2 mb-2">
           <img src={logo} alt="" className="w-[100px]" />
         </div>
 
         <div
-          onClick={() => setIsMenuOpen(!isMenuOpen)}
-          className="text-3xl absolute right-8 top-4 cursor-pointer md:hidden"
+          className="lg:flex items-center justify-between overflow-hidden border border-solid border-black w-[50%] rounded-full px-4 py-1
+          hidden"
         >
-          {isMenuOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
-        </div>
-
-        <div
-          className={`flex overflow-hidden items-center px-2 bg-white border-2 border-black rounded-[30px] max-w-[719px]
-          ${isSearchOpen ? "w-full" : "hidden md:flex"}`}
-        >
-          {/* ml-3 mt-5 sm:mb-5 max-w-[719px] */}
+          <div className="cursor-pointer mr-1">
+            <AiOutlineSearch size={28} />
+          </div>
           <div className="w-full mr-2">
             <input
               type="text"
-              className="p-2 w-full border-none outline-none"
+              placeholder="Search"
+              className="p-1 w-full text-lg outline-none bg-gray-200"
             />
           </div>
-          <div onClick={handleSearchClick} className="cursor-pointer">
-            <AiOutlineSearch size={32} />
+        </div>
+
+        <div className="lg:hidden">
+          <div
+            className="cursor-pointer mr-1"
+            onClick={() => setIsSearchOpen(!isSearchOpen)}
+          >
+            {isSearchOpen ? (
+              <AiOutlineClose size={32} />
+            ) : (
+              <AiOutlineSearch size={32} />
+            )}
           </div>
+          {isSearchOpen && (
+            // <div className="flex items-center px-2 bg-white overflow-hidden border border-black rounded-full md:absolute md:top-20 top-[-490px] w-full md:w-auto transition-all duration-500 ease-in">
+            <div className="flex items-center px-2 bg-white overflow-hidden border border-black rounded-full absolute top-20 lg:top-[-490px] w-auto transition-all duration-500 ease-in">
+              <input
+                type="text"
+                className="p-2 w-full border-none outline-none"
+                placeholder="Search"
+              />
+            </div>
+          )}
         </div>
 
         <div
           className={`cursor-pointer md:flex md:items-center md:pb-0 pb-12 absolute md:static md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in 
-          ${isMenuOpen ? "top-32 opacity-100" : "top-[-490px]"} md:opacity-100`}
+          ${isMenuOpen ? "top-14 opacity-100" : "top-[-490px]"} md:opacity-100`}
         >
-          {/* top-12 */}
           {navItems.map((v, key) => (
             <div
               key={key}
               className="md:ml-8 text-xl font-semibold md:my-0 my-7"
             >
-              <div className="flex items-center text-black-dark hover:text-black-light duration-500 text-center">
-                <div className="mr-1">{v.icon}</div>
-                <div>
+              <div className="flex items-center duration-500 text-center">
+                {/* <div className="mr-1">{v.icon}</div> */}
+                {/* <div> */}
                   <Link to={v.path}>{v.label}</Link>
-                </div>
+                {/* </div> */}
               </div>
             </div>
           ))}
@@ -74,6 +93,14 @@ const Nav = () => {
 
         <div className="w-[50px]">
           <img src={userIcon} alt="" />
+        </div>
+
+        <div
+          onClick={() => setIsMenuOpen(!isMenuOpen)}
+          className="text-3xl cursor-pointer md:hidden"
+          // absolute right-8 top-5
+        >
+          {isMenuOpen ? <AiOutlineClose /> : <GiHamburgerMenu />}
         </div>
       </div>
     </nav>
